32fcbc1a0011d6256f7d8ed2d36ca08adfdd8003
Daniel Fernandes <777ferna@gmail.com>
2026-03-31 18:24:18 -0300

    try except

--- try_except.py
+++ try_except.py
@@ -1,12 +1,33 @@
 try:
     numero = 6
-adv = int(input("Adivinhe o número: "))
-
-while adv != numero:
-    print("Número incorreto, tente novamente!")
     adv = int(input("Adivinhe o número: "))
-else:
-    print("Parabéns, você acertou o número!")
+
+    while adv != numero:
+        print("Número incorreto, tente novamente!")
+        adv = int(input("Adivinhe o número: "))
+    else:
+        print("Parabéns, você acertou o número!")
 
 except ValueError:
-print("Este numero não é inteiro.")
+    print("Este numero não é inteiro.")
+
+# --------------------------------------------------- Except divisao por 0
+
+try:
+    numero = 10 / 0
+    print(numero)
+
+except ZeroDivisionError:
+    print("O calculo nao pode ser divisivel por 0")
+
+# ------------------------------------------------------ Except TypeError
+
+try:
+    numero = "10" + 3
+    print(numero)
+
+except TypeError:
+    print("Erro, não pode ser calculado com uma string")
+
+finally:
+    print("Fim do programa!")
