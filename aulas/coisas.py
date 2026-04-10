aa024a23671041de3f188531131dc63964a382f9
dan <frnandes@icloud.com>
2026-04-10 17:06:30 -0300

    att

--- aulas/coisas.py
+++ aulas/coisas.py
@@ -1,7 +1,20 @@
-import datetime
+class Pessoa:
+    def __init__(self, nome, idade, gosto):
+        self.nome = nome
+        self.gosto = gosto
+        self. idade = idade
 
-data_atual = datetime.date.today()
-print(data_atual)
+    def saudacao(self):
+         return f"\nHi, my name is {self.nome}, i have {self.idade} years, and i like {self.gosto}."
 
-hora_atual = datetime.datetime.now()
-print(hora_atual)
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+gosto = input("Digite algo que gosta: ")
+
+pessoa1 = Pessoa(nome, idade, gosto)
+
+print(pessoa1.saudacao())
+
+with open('save.py', 'a') as arquivo:
+    arquivo.write(f"\nHi, my name is {nome}, i have {idade} years, and i like {gosto}.")
---
+++ aulas/save.py
@@ -0,0 +1,1 @@
+Hi, my name is Daniel, i have 18 years, and i like game.
